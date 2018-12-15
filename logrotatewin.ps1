df572083b5a29c464246d72e2be0649cc3916b92
Marcus Teixeira <40602844+devopsrj@users.noreply.github.com>
2018-12-15 11:25:16 -0200

    Create logrotatewin.ps1

---
+++ logrotatewin.ps1
@@ -0,0 +1,22 @@
+# Define where Application writes the log files
+$logFiles = "C:\path\"
+
+# Define the location the log files should be archived to
+$logArchive = "C:\path\"
+
+# Define for how many days we should retain logs in the archive location
+[int] $KeepRaw = 1
+#$logLimitarchive = (Get-Date).day
+$logLimitdelete = (Get-Date).AddDays(-7)
+$CurrentDate = Get-Date -Hour 0 -Minute 0 -Second 0
+$CompressBefore = (Get-Date -Date $CurrentDate).AddDays( - $KeepRaw)
+
+# Datestamp the current log files and move them to the archive location
+Get-ChildItem -Path $logFiles -Filter * |Where-Object {$_.LastWriteTime -lt $CompressBefore} |  ForEach-Object {
+	$newName = "$($_.DirectoryName)\$(Get-Date -Format yyyy-MM-dd)-$($_.BaseName)$($_.Extension)"
+	Rename-Item -Path $_.FullName -NewName $newName
+	Move-Item -Path $newName -Destination $logArchive
+	}
+
+# Delete log files older than the defined number of days
+Get-ChildItem -Path $logArchive | Where-Object {$_.LastWriteTime -lt $logLimitdelete} | Remove-Item -Force
